22bb91fb94dd617ba8d287f9c7d94a7fc39e14a1
Hisham Khattab <42070333+hishamkhattab@users.noreply.github.com>
2021-10-01 23:38:32 +0200

    uploading design

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// let hamburguerIcon = document.querySelector(".header .container .links .icon");
+// let navLinks = document.querySelector(".header .container .links ul");
+
+
+
+  
+// hamburguerIcon.addEventListener("click", (event) => {
+//     console.log(event.target);
+//     // navLinks.classList.add('display-block')
+// })
